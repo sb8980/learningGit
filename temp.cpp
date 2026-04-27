a9dc6070de95cabde9d066e8ce79079ba9d10bfc
sb8980 <somdipta.x@gmail.com>
2026-04-27 16:46:23 +0530

    commit

--- temp.cpp
+++ temp.cpp
@@ -4,8 +4,6 @@ int main() {
     std::cout << "Hello World!";
     std::cout << "Hello World!";
     std::cout << "Hello World!";
-    std::cout << "Hello World!";
-    std::cout << "Hello World!";
-    std::cout << "Hello World!";
+
     return 0;
 }
